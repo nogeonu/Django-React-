63158588ed6ce5ef8e26a8cd7028ae91c80f9a47
nogeonu <nogeonu@users.noreply.github.com>
2025-10-31 10:35:40 +0900

    코드정리.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 5000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8001',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
