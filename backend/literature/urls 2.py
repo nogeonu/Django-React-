8d8d9bf17362445262085617a6c8e5c9d42a23af
nogeonu <nogeonu@users.noreply.github.com>
2025-11-03 12:24:35 +0900

    fix: remove duplicate files

--- backend/literature/urls 2.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import LitSearchView, LitOpenAccessView, NewsFeedView
-
-urlpatterns = [
-    path("search", LitSearchView.as_view()),
-    path("openaccess", LitOpenAccessView.as_view()),
-    path("news", NewsFeedView.as_view()),
-]
