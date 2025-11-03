e639026c4ed343abe341f52dd361ccfe3fd62126
nogeonu <nogeonu@users.noreply.github.com>
2025-11-03 20:38:21 +0900

    fix: disable timezone conversion for MySQL timestamps (USE_TZ=False)

--- backend/eventeye/settings.py
+++ backend/eventeye/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'ko-kr'
 TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
