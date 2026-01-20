fac036fa8c05e3dbe641d212d0a9d3aebc8bd11d
nogeonu <nogeonu@users.noreply.github.com>
2026-01-20 22:46:04 +0900

    feat: RedisChannelLayer 도입으로 웹소켓 안정화 및 서버 연동 최신화

--- backend/eventeye/settings.py
+++ backend/eventeye/settings.py
@@ -85,7 +85,10 @@ ASGI_APPLICATION = 'eventeye.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
